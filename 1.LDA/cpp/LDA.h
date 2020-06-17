264e9db3c18af88a399f5f3408071b342326f5c8
Christian Leininger <info2016frei@gmail.com>
2020-06-17 18:36:41 +0200

    add begining h file

---
+++ 1.LDA/cpp/LDA.h
@@ -0,0 +1,28 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+#ifndef LDA_H_
+#define LDA_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+
+class LDA {
+ public:
+  LDA();
+  ~LDA();
+ private:
+  // std::vector<std::string> _trainData;
+  int _trainDataSize;
+  
+};
+
+#endif // LDA_H_
+
