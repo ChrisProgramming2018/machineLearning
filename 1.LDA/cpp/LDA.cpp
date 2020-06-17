3f2a052119a0d3b3f8e78a153b493054018dd57f
Christian Leininger <info2016frei@gmail.com>
2020-06-17 18:36:22 +0200

    add begining

---
+++ 1.LDA/cpp/LDA.cpp
@@ -0,0 +1,12 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include "./LDA.h"
+
+// ______________________________________________________________
+LDA::LDA() {
+  _trainDataSize = 0;
+}
+
